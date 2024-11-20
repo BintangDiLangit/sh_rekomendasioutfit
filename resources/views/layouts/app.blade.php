b539c03f9ccd4b52f53b335e70a9d8a1a6246a5e
BintangDiLangit <supercellbanned1@gmail.com>
2024-11-20 12:30:03 +0700

    finishng

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -193,38 +193,6 @@
 </head>
 
 <body>
-    <!-- Optional Navigation -->
-    @auth
-        <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Enchanted Trinkets') }}
-                </a>
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('admin.products.index') }}">Products</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('admin.categories.index') }}">Categories</a>
-                        </li>
-                        <li class="nav-item">
-                            <form action="{{ route('logout') }}" method="POST">
-                                @csrf
-                                <button type="submit" class="nav-link btn btn-link">Logout</button>
-                            </form>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    @endauth
-
     <!-- Main Content -->
     <main>
         @yield('content')
